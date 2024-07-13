3243c7b99634319eaa0e0709496acaac003e5078
Kuldeep Modi <kuldeepmodi95@gmail.com>
2024-07-13 10:51:17 +0530

    initial setup commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="font-bold h-screen flex justify-center items-center">Are You Time Traveller???</h1>
+  );
+}
