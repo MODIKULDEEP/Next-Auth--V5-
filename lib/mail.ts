fbac0a59853edcd40a4014ad7d5d5f18cdd3c889
Kuldeep <kuldeepmodi95@gmail.com>
2024-07-18 18:25:31 +0530

    build issue solved

--- lib/mail.ts
+++ lib/mail.ts
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const mailUser = process.env.MAIL_USERNAME;
-const domain = process.env.NEXT_PUBLIC_SERVICE_URL;
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
